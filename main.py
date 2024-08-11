51bd6a5830b21f118da44cbedeadcaa53aa0fdb2
Andres Carro <acarro@itba.edu.ar>
2024-08-11 19:52:45 -0300

    Add ej2d config and simulation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 from src.plots import *
 
-
+FILENAME_EJ2D = 'ej2d.csv'
 FILENAME_EJ1 = "results/ej1.csv"
 
 
@@ -28,6 +28,37 @@ def ej1(ball_throws=100):
                 probs.append(accumulated / ball_throws)
 
             writer.writerow([pokemon["pokemon"], *probs])
+
+
+def ej2d():
+    factory = PokemonFactory("pokemon.json")
+    with open(f"{sys.argv[1]}", "r") as f, open(FILENAME_EJ2D, mode='w', newline='') as output:
+        config = json.load(f)
+        writer = csv.writer(output)
+        writer.writerow(['Status Effect', 'Pokeball Prob', 'Ultraball Prob', 'Heavyball Prob', 'Fastball Prob'])
+        for pokemon in config:
+            current_pokemon = None
+            for ball in pokemon["pokeball"]:
+                probs = []
+                match pokemon.status_effect:
+                    case 'NONE':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.NONE, 1)
+                    case 'FREEZE':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.FREEZE, 1)
+                    case 'POISON':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.POISON, 1)
+                    case 'BURN':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.BURN, 1)
+                    case 'PARALYSIS':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.PARALYSIS, 1)
+                    case 'SLEEP':
+                        current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.SLEEP, 1)
+                    case _:
+                        raise Exception(f'Unknown status effect {pokemon.status_effect}')
+                catched, prob = attempt_catch(current_pokemon, ball)
+                probs.append(prob)
+
+            writer.writerow([pokemon["status_effect"], *probs])
 
 
 if __name__ == "__main__":
