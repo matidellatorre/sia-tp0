dc9a379e557ef2f3429596c016e0d86559c8a52b
Alejo Flores Lucey <alejo@misterflores.com>
2024-08-11 19:09:06 -0300

    Add implementation of ex. 1b. Minor changes in current implementation to match use

--- main.py
+++ main.py
@@ -4,30 +4,33 @@ import csv
 
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
-from src.plots import plot_ej1a
+from src.plots import *
 
-FILENAME_EJ1A = 'ej1a.csv'
 
-def ej1a():
+FILENAME_EJ1 = "results/ej1.csv"
+
+
+def ej1(ball_throws=100):
     factory = PokemonFactory("pokemon.json")
-    with open(f"{sys.argv[1]}", "r") as f, open(FILENAME_EJ1A, mode='w', newline='') as output:
+    with open(f"{sys.argv[1]}", "r") as f, open(FILENAME_EJ1, mode="w", newline="") as output:
         config = json.load(f)
         writer = csv.writer(output)
-        writer.writerow(['Pokemon', 'Pokeball Prob', 'Ultraball Prob', 'Heavyball Prob', 'Fastball Prob'])
+        writer.writerow(["pokemon", "pokeball", "ultraball", "heavyball", "fastball"])
         for pokemon in config:
             probs = []
             for ball in pokemon["pokeball"]:
                 accumulated = 0
-                for i in range(100):
+                for _ in range(ball_throws):
                     current_pokemon = factory.create(pokemon["pokemon"], 100, StatusEffect.NONE, 1)
-                    catched, _prob = attempt_catch(current_pokemon, ball)
+                    catched, _ = attempt_catch(current_pokemon, ball)
                     if catched:
                         accumulated += 1
-                probs.append(accumulated/100)
+                probs.append(accumulated / ball_throws)
 
             writer.writerow([pokemon["pokemon"], *probs])
 
 
 if __name__ == "__main__":
-    ej1a()
-    plot_ej1a(FILENAME_EJ1A)
+    # ej1(1000)
+    # plot_ej1a(FILENAME_EJ1)
+    plot_ej1b(FILENAME_EJ1)
--- src/plots.py
+++ src/plots.py
@@ -5,19 +5,19 @@ import numpy as np
 
 def plot_ej1a(filename):
     df = pd.read_csv(filename)
-    pokemons = df['Pokemon']
+    pokemons = df["pokemon"]
     balls = {
-        'Pokeball': df['Pokeball Prob'],
-        'Heavyball': df['Heavyball Prob'],
-        'Fastball': df['Fastball Prob'],
-        'Ultraball': df['Ultraball Prob'],
+        "Pokeball": df["pokeball"],
+        "Heavyball": df["heavyball"],
+        "Fastball": df["fastball"],
+        "Ultraball": df["ultraball"],
     }
 
     x = np.arange(len(pokemons))
     width = 0.2
     multiplier = 0
 
-    fig, ax = plt.subplots(layout='constrained')
+    _, ax = plt.subplots(layout="constrained")
 
     for attribute, measurement in balls.items():
         offset = width * multiplier
@@ -25,10 +25,46 @@ def plot_ej1a(filename):
         ax.bar_label(rects, padding=4)
         multiplier += 1
 
-    ax.set_ylabel('Probabilidad de captura')
+    ax.set_ylabel("Probabilidad de captura")
     ax.set_xticks(x + 1.5 * width, pokemons.map(lambda x: x.capitalize()))
-    ax.legend(loc='upper left', ncols=4)
+    ax.legend(loc="upper left", ncols=4)
     ax.set_ylim(0, 1)
     ax.set_yticks(np.arange(0, 1.1, 0.1)) 
 
+    plt.show()
+
+
+def plot_ej1b(filename):
+    df = pd.read_csv(filename)
+    
+    fig, ax = plt.subplots(nrows=2, ncols=3, layout="constrained")
+    
+    positions = [[0,0], [0,1], [0,2], [1,0], [1,1]]
+    
+    for i, row in df.iterrows():
+        axis = ax[*positions[i]]
+        
+        pokeball_probability = row["pokeball"]
+        ultraball_probability = row["ultraball"]
+        heavyball_probability = row["heavyball"]
+        fastball_probability = row["fastball"]
+        
+        balls = {
+            "Ultraball": ultraball_probability / pokeball_probability,
+            "Heavyball": heavyball_probability / pokeball_probability,
+            "Fastball": fastball_probability / pokeball_probability
+        }
+        
+        for j, (ball, probability) in enumerate(balls.items()):
+            rect = axis.bar(j + 1, probability, label=ball)
+            axis.bar_label(rect, padding=2)
+            axis.axhline(y=1, color='k', linestyle='--')
+
+        
+        axis.set_ylabel("Efectividad de captura comparado con Pokeball")
+        axis.set_xlabel(row["pokemon"].capitalize())
+        axis.legend(loc="lower left", ncols=3)
+        axis.set_xticks([])
+        
+    fig.delaxes(ax[1, 2])
     plt.show()
